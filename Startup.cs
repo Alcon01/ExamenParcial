3433450cb90ac36ac7c689b07ed6bd0160cba40d
JHOSEN JHONNY ALCON QUISPE <nieroinge5@gmail.com>
2021-09-14 13:06:14 -0400

    Agregar archivos de proyecto.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamenParcial.Startup))]
+namespace ExamenParcial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
